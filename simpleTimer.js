2c14bbda33dee80d85ce45b8de8cce5089964bf7
asearer <amsearer84@gmail.com>
2023-07-16 08:09:02 -0400

    On branch master  Initial commit  Changes to be committed: 	new file:   simpleTimer.js

---
+++ simpleTimer.js
@@ -0,0 +1,9 @@
+function startTimer(duration) {
+    setTimeout(function() {
+      alert('Timer finished!');
+    }, duration);
+  }
+  
+  // Usage example: Start a timer for 5 seconds (5000 milliseconds)
+  startTimer(5000);
+  
